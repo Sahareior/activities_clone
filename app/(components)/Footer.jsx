6f870956f98bc2dcbe1c36cda1b1538eb617ccbc
Sahareior Sijan <sahareiors@gmail.com>
2025-04-04 18:48:01 +0600

    finished for now

--- app/(components)/Footer.jsx
+++ app/(components)/Footer.jsx
@@ -6,17 +6,21 @@ const Footer = () => {
         <div className=' max-w-5xl mb-16 mx-auto bg-white'>
             <footer >
   <div className='flex flex-col text-slate-500  py-9'>
-    <div className='flex justify-cente gap-2'>
+    <div className='flex justify-center items-center gap-2'>
     <p className='text-[12px]'>This experience is powered by</p>
         {/* <img src="https://thechildunplugged.zbooni.com/_next/static/media/zbooni-logo-black.ca8163a2.svg" alt="" /> */}
    
-        <Image
- src="https://thechildunplugged.zbooni.com/_next/static/media/zbooni-logo-black.ca8163a2.svg"
+
+
+<Image
+   src="https://thechildunplugged.zbooni.com/_next/static/media/zbooni-logo-black.ca8163a2.svg"
   alt="Zbooni Logo"
-  width={150} // Set explicit width
-  height={50} // Set explicit height
+  width={150}
+  height={50}
+  style={{ width: '150px', height: '30px', objectFit: 'contain' }}
   priority
 />
+
 
     </div>
 
