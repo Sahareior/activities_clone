3edc6de3316a44c93c60a2c80530fa9a0b906894
Sahareior Sijan <sahareiors@gmail.com>
2025-04-08 22:57:29 +0600

    updated loading

--- app/(components)/Items.jsx
+++ app/(components)/Items.jsx
@@ -25,14 +25,20 @@ const Items = () => {
   }, [setActiveTab, activeTab]);
 
   const handleFetch = async () => {
-    const response = await axios.get("https://server-sijans-projects-f3bcab8f.vercel.app/allproducts");
-    setData(response.data);
+    try {
+      const response = await axios.get("https://server-sijans-projects-f3bcab8f.vercel.app/allproducts");
+      setData(response.data);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    } finally {
+      setLoading(false); // Ensure loading is turned off after API call completes (success or fail)
+    }
   };
-
+  
   useEffect(() => {
     handleFetch();
-    setLoading(false)
   }, []);
+  
 
   // Filter data based on active tab
   let finalData = [];
