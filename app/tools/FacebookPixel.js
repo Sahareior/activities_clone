4b7b8e9406a8e1657c03c15fdbb2f2f7769dd44b
Sahareior Sijan <sahareiors@gmail.com>
2025-04-22 20:14:49 +0600

    ddd

--- app/tools/FacebookPixel.js
+++ app/tools/FacebookPixel.js
@@ -1,3 +1,5 @@
+'use client'; // Add this directive since we're using browser APIs
+
 import Script from 'next/script'
 
 const FacebookPixel = () => {
@@ -6,18 +8,20 @@ const FacebookPixel = () => {
       <Script
         id="fb-pixel"
         strategy="afterInteractive"
+        onLoad={() => {
+          console.log('Facebook Pixel loaded successfully');
+          fbq('init', '2128380290964389');
+          fbq('track', 'PageView');
+        }}
+        src="https://connect.facebook.net/en_US/fbevents.js"
+      />
+      <Script
+        id="fb-pixel-init"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2128380290964389');
-            fbq('track', 'PageView');
+            window._fbq = window._fbq || [];
+            window._fbq.push(['track', 'PageView', {}]);
           `,
         }}
       />
